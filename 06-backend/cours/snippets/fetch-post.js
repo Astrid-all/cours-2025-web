7855cac6901898ea8c658b8a2449685785c49a70
Raphael Prasquier <raphael@imagine-app.fr>
2026-03-31 13:42:54 +0200

    Update fetch-post snippet endpoint

--- 06-backend/cours/snippets/fetch-post.js
+++ 06-backend/cours/snippets/fetch-post.js
@@ -1,6 +1,6 @@
 // Faire une requête POST avec fetch
 
-const response = await fetch("https://httpbin.org/post", {
+const response = await fetch("http://localhost:3000/contact", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ name: "Ada", message: "Bonjour" }),
